56526a1ac90227f93e7f1eb7f90b2c5711fab7a5
Yeehen <1297436022@qq.com>
2020-07-21 23:07:38 +0800

    countingbits

--- code/leetcode/src/chapter14/CountingBits.java
+++ code/leetcode/src/chapter14/CountingBits.java
@@ -1,5 +1,15 @@
 package chapter14;
 
+import java.util.Arrays;
+
 class CountingBits {
-    //动态规划 + 位运算    
+    //动态规划 + 位运算
+    public int[] countBits(int num) {
+        int[] bits = new int[num + 1];
+        Arrays.fill(bits, 0);
+        for (int i = 1; i <= num; ++i) {
+            bits[i] += bits[i & (i - 1)] + 1;
+        }
+        return bits;
+    }    
 }
